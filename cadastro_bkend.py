d808d16a1c34c9a9c3957ec31f8c3bcbb708d1b2
juguimaraes <juju.guimaraes@gmail.com>
2017-09-14 09:11:50 -0300

    alterações no backend e schema bd

--- cadastro_bkend.py
+++ cadastro_bkend.py
@@ -1,24 +1,30 @@
 from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemy
 
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_PORT'] = 3307
+app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'usbw'
-app.config['MYSQL_DATABASE_DB'] = 'cadastro1'
+app.config['MYSQL_DATABASE_DB'] = 'timetable_db'
 
 mysql.init_app(app)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def index():
+    return render_template('cad_user.html')
+
+@app.route('/add', methods=['POST'])
+def save_user():
 	usuario = request.form['usuario']
 	senha = request.form['senha']
 	email = request.form['email']
 	nome = request.form['nome']
 	cur = mysql.get_db().cursor()
-	cur.execute('''INSERT INTO cadastro1 (Nome, Usuario, Senha, Email) VALUES (%s, %s, SHA1(%s), %s) ''', (nome, usuario, senha, email))
+	cur.execute('''INSERT INTO usuarios (Nome, Usuario, Senha, Email) VALUES (%s, %s, SHA1(%s), %s) ''', (nome, usuario, senha, email))
 
 if __name__ == '__main__':
 	app.run(debug=True)
